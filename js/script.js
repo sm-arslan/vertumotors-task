34963508241494386638807bb219d2444043a682
aslan <sn20adh@herts.ac.uk>
2023-06-20 15:40:28 +0100

    update js script

--- js/script.js
+++ js/script.js
@@ -1,55 +1,71 @@
+
 if (typeof jQuery === 'undefined') {
     throw new Error(' JavaScript requires jQuery')
 }
 
-var screenWidth = $(document).width(); 
-//click on hamburger icon to show menu
-$('.navbar-toggler').on('click', (e)=>{
+//initialize the variables with elements
+const hamburgerIcon = document.getElementsByClassName('navbar-toggler');
+const closeIconForMenu = document.getElementsByClassName('offcanvas-close');
+const searchIcon = document.getElementsByClassName('sm-search-icon');
+const closeIconForSearch = document.getElementsByClassName('search-close');
+
+
+hamburgerIcon[0].addEventListener("click", (e) => {
     //add class
-    $('.modal-backdrop').addClass('d-show');
+    addClass(document.getElementsByClassName('modal-backdrop')[0], 'd-show');
     //show menu
-    changeCSS('.offcanvas', 'transform', 'none');
-    //
-    changeCSS('.offcanvas', 'visibility', 'visible');
-    
+    changeCSS('offcanvas', 'transform', 'none', null);
+    changeCSS('offcanvas', 'visibility', 'visible', null);
+
 });
 //click on hamburger icon to hide menu
-$('.offcanvas-close').on('click', (e)=>{
+closeIconForMenu[0].addEventListener("click", (e) => {
     //remove class
-    $('.modal-backdrop').removeClass('d-show');
+    removeClass(document.getElementsByClassName('modal-backdrop')[0], 'd-show');
     //hide menu
-    changeCSS('.offcanvas', 'transform', '');
-    //
-    changeCSS('.offcanvas', 'visibility', '');
+    changeCSS('offcanvas', 'transform', '', null);
+    changeCSS('offcanvas', 'visibility', '', null);
 });
 
 
-//
-$('.sm-search-icon').on('click', (e)=>{
-    changeCSS('.container-search-outer', 'width', '100%');
-    changeCSS('.container-search-outer', 'transform', 'none');
-    changeCSS('.search-close', 'display', 'block');
-    
-    $('.container-search').attr('style', 'display: flex !important');
-    
-    changeCSS('.navbar-toggler', 'display', 'none');
-    changeCSS('.navbar-brand', 'display', 'none');
-    changeCSS('.navbar-nav-right', 'display', 'none');
-    
-});
-$('.search-close').on('click', (e)=>{
-    changeCSS('.container-search-outer', 'width', 'auto');
-    changeCSS('.container-search-outer', 'transform', '');
-    changeCSS('.search-close', 'display', '');
-    changeCSS('.container-search', 'display', '');
+//show the search bar, provided screen size is less then 768px
+searchIcon[0].addEventListener("click", (e) => {
+    changeCSS('container-search-outer', 'width', '100%', null);
+    changeCSS('container-search-outer', 'transform', 'none', null);
+    changeCSS('search-close', 'display', 'block', null);
+    changeCSS('container-search', 'display', 'flex', 'important');
+    changeCSS('navbar-toggler', 'display', 'none', null);
+    changeCSS('navbar-brand', 'display', 'none', null);
+    changeCSS('navbar-nav-right', 'display', 'none', null);
 
-    changeCSS('.navbar-toggler', 'display', '');
-    changeCSS('.navbar-brand', 'display', '');
-    changeCSS('.navbar-nav-right', 'display', '');
+});
+//close the search bar, provided screen size is less then 768px
+closeIconForSearch[0].addEventListener("click", (e) => {
+    changeCSS('container-search-outer', 'width', 'auto', null);
+    changeCSS('container-search-outer', 'transform', '', null);
+    changeCSS('search-close', 'display', '', null);
+    changeCSS('container-search', 'display', '', null);
+    changeCSS('navbar-toggler', 'display', '', null);
+    changeCSS('navbar-brand', 'display', '', null);
+    changeCSS('navbar-nav-right', 'display', '', null);
 
 });
 
-//function to change the css property of an element 
-function changeCSS(classname, property, value){
-    $(classname).css(property, value);
+//to change the css property of an element 
+function changeCSS(classname, property, value, rule) {
+    if (rule != null) {
+        document.getElementsByClassName(classname)[0].style.setProperty(property, value, rule);
+    } else {
+        document.getElementsByClassName(classname)[0].style.setProperty(property, value);
+    }
+}
+
+//to add class on element
+function addClass(element, classname) {
+    element.classList.add(classname);
+}
+
+//to remove class on element
+function removeClass(element, classname) {
+    element.classList.remove(classname);
 }
